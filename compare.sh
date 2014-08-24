064edd67e0535f01a98282505e34ce02eb03befa
F3RR3T <github@st33v.com>
2014-08-24 14:26:51 +1000

    Interim stage. 	Monitor script logs every 30 mins. 	compare.sh has some test file reading and comparison functions But the time has come to wrap it all into one script. This will be called monitor.sh

---
+++ compare.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -
+# SJP August 2014
+# Reads logfile of IP address records created by monitor.sh
+# When IP address changes it sends an email"
+
+
+cd ~/mail/monitor
+
+if (test -e data/tmp.*) then
+	datafile=$(ls data/tmp.*)
+else
+	exit
+fi
+
+
+#i=0
+#while read line
+#do
+#	i=$[i+1]
+#	echo $i, $line
+#done < $datafile 
+
+# Get the IP address from the first line 
+read firstline < $datafile
+
+firstip=$(echo $firstline |  awk '{ print $2 }')
+
+# sec=$(echo $firstline |  awk '{ print $1 }')
+
+echo "First = $firstip"
+
+lastline=$(tail -1 $datafile)
+
+lastip=$(echo $lastline | awk '{ print $2 }')
+
+echo "Last = $lastip"
+
+[ "$lastip" = "$firstip" ] && echo 'match' || echo 'different'
--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,4 @@
-#! /bin/sh -
+#! /bin/bash -
 
 #  SJP 16 August 2014
 #  Monitor my external IP address
@@ -13,10 +13,14 @@ else
 	datafile=`mktemp --tmpdir=data`
 fi
 
-echo "datafile = $datafile"
+# echo "datafile = $datafile"
 
 ip=$(curl ifconfig.me/ip)
+[ ${#ip} = 0 ] && ip="Timed out"
+# Could also do [ -z $ip ]
+ 
 dt=$(\date  +%Y-%m-%dT%T%:z)
+
 
 echo -e "$dt\t$ip" >> $datafile
 
